77c52e4f5e4e0e1f36b6ea1d47927eae3a227b40
Ari <arireedy42@gmail.com>
2020-02-19 15:40:05 -0800

    more test cases

--- test_main.c
+++ test_main.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 00:36:23 by rreedy            #+#    #+#             */
-/*   Updated: 2020/02/13 21:32:58 by rreedy           ###   ########.fr       */
+/*   Updated: 2020/02/19 14:09:25 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,66 @@ int		main()
 {
 	char	*str;
 
-	str = malloc(5);
-	strcpy(str, "test");
-	printf("%s\n", str);
-	realloc(str, 9);
-	strcpy(str, "new test");
-	printf("%s\n", str);
-	realloc(str, 20);
-	strcpy(str, "new test with new :)");
-	printf("%s\n", str);
+	str = malloc(11);
+	strcpy(str, "tiny malloc");
+	printf("%p: %s\n", str, str);
+	str = realloc(str, 13);
+	strcpy(str, "tiny malloc 2");
+	printf("%p: %s\n", str, str);
+	str = realloc(str, 28);
+	strcpy(str, "small malloc 1. is it small?");
+	printf("%p: %s\n", str, str);
+	str = realloc(str, 34);
+	strcpy(str, "small malloc 2. is it STILL small?");
+	printf("%p: %s\n", str, str);
+	str = realloc(str, 16);
+	strcpy(str, "tiny malloc 3");
+	printf("%p: %s\n", str, str);
 	str = realloc(str, 1000);
 	strcpy(str, "v65QWQ6qBFJzWIvAxB3s8sOfROWWmunixDCrkX56x4gvAm9G0DqeMJewRC9A3gJbwyMqSt7Ip0k4AfMwrFRtFgUqbH0boYHRqD3rQ3CCJDHINadB6Y0MxC4q0M8zj4RlQeivx5tQiTraRKgsq85aB4UABPFWjnHCkPw1CDFzcnfjxgBAn4n1Fpwp9YHKY0t0MN2vgTGCQUHowChJSI92PtDy6tNA0qNcETTtji2OoSc2ZmGpC7aPDxTH52QaLeGDxbLfiFz9UXxeIbs5Km14tVURxP50ILN7fZl8DaG7wVJLFkOZtHFoD32GrQAoupjAufKLnXeLeutVc71n50nKUWdRDjLpTlvnYNOocTQqfODmdKpS4Tb9sBAheMPbGTwA7w9mRM3aguUNgORks2OqFW2d5n4fvmSkKzmZepfSmLCccJUA6J4b0LAX6grFzpLJdKLdf2Jt8wrqEu8UFzf17obEogLuhn8J5bGFVd2w414RIfdNJh4NuurIfbR8CYDj4cbMocV8dqAmK6MPA4cWXiBbHdgqPYtBWfmbtdwjO9mvGwlkQ6L25QxTq1ZCIPf23vX96fyBGdsY1nfNbGvTjSHQWeBxZJgPaZlPtDiKiKn6pKqweRzESQ20LLbExvGwoALviArZSJ24vRMzMWoyAvpyfIliVK7p89v99Fc8t4nRu77x9tSpBMWWiH7DKFfvzsfYiMdgNnNBSI5P5lxOnr8KW1HHd4Y2BTwy4P6VL0n3zDiBEVRpVj5kgxHriAev6z7tkIZilBtykh07l5LYXDTq90jkAMLxfCtIlqv8YNm8Y6zF71ybSkOoFuZSV4Cn8KvSTbGb9lcuSEJQ2j0xlcoS7s70Pfwp4EHqx7pgl8Xy8WUkl0C4RrbuOlI8rh2Yo83s51VKbycTJDQ3XC4Es4rsG97msthRoUROWq8VysLSXk7eWiNMcWZOpz5A9XRfsMKdBEfY1c6ssprEQl6ygJjq5hwzM1Zlf2bR2B1");
-	printf("%s\n", str);
+	printf("%p: %s\n", str, str);
+
+	char	**allocs;
+
+	allocs = (char **)malloc(sizeof(char *) * 500);
+	for (int i = 0; i < 500; i++)
+	{
+		printf("TINY TEST #%d\n", i + 1);
+		allocs[i] = malloc(16);
+		memset(allocs[i], (i + 1) % 26 + 97, 15);
+		allocs[i][15] = '\0';
+		printf("%p: %s\n", allocs[i], allocs[i]);
+	}
+	for (int i = 0; i < 500; i++)
+	{
+		free(allocs[i]);
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		printf("SMALL TEST #%d\n", i + 1);
+		allocs[i] = realloc(allocs[i], 500);
+		memset(allocs[i], (i + 1) % 26 + 97, 499);
+		allocs[i][499] = '\0';
+		printf("%p: %s\n", allocs[i], allocs[i]);
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		free(allocs[i]);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("LARGE TEST #%d\n", i + 1);
+		allocs[i] = realloc(allocs[i], 1000);
+		memset(allocs[i], (i + 1) % 26 + 97, 999);
+		allocs[i][999] = '\0';
+		printf("%p: %s\n", allocs[i], allocs[i]);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		free(allocs[i]);
+	}
+	free(allocs);
+	printf("test\n");
 	return (0);
 }
