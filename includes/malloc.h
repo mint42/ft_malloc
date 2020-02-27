cb48e78c4aa8ef5c62b16e8ffba9237f56c678ae
Ari <arireedy42@gmail.com>
2020-02-26 19:53:31 -0800

    removing comments and adding calloc

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 18:47:57 by rreedy            #+#    #+#             */
-/*   Updated: 2020/02/19 16:44:29 by rreedy           ###   ########.fr       */
+/*   Updated: 2020/02/26 19:32:48 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,9 @@
 # define SML_ALLOC_SIZE 512
 # define NPAGES_OVERHEAD 3
 
-///*
-//**	>:(		DO NOT CONFIGURE:
-//**
-//**	*_PG_PAD	-> Padding will be needed to align the allocations evenly
-//**	*_PG_SPACE	-> How much of the page is free for the allocations considering
-//**                 getpagesize(), the tiny/small page header size, and padding
-//*/
-//# define PAGESIZE 4096
-//# define TNY_PG_PAD ((PAGESIZE - TS_PGHEADR_SIZ) % (TNY_ALLOC_SIZE + TS_ALHEADR_SIZ))
-//# define SML_PG_PAD ((PAGESIZE - TS_PGHEADR_SIZ) % (SML_ALLOC_SIZE + TS_ALHEADR_SIZ))
-//# define TNY_PG_OFSET (TS_PGHEADR_SIZ + TNY_PG_PAD)
-//# define SML_PG_OFSET (TS_PGHEADR_SIZ + SML_PG_PAD)
-//# define TNY_PG_SPACE (PAGESIZE - TNY_PG_OFSET)
-//# define SML_PG_SPACE (PAGESIZE - SML_PG_OFSET)
-//# define N_TNY_ALOCS_PER_PG (TNY_PG_SPACE / (TS_ALHEADR_SIZ + TNY_ALLOC_SIZE))
-//# define N_SML_ALOCS_PER_PG (SML_PG_SPACE / (TS_ALHEADR_SIZ + SML_ALLOC_SIZE))
+/*
+**	>:(		DO NOT CONFIGURE:
+*/
 # define TINY 1
 # define SMALL 2
 # define LARGE 3
@@ -50,6 +37,7 @@
 struct g_malloc	*info;
 
 void			*malloc(size_t size);
+void			*calloc(size_t num, size_t size);
 void			*realloc(void *ptr, size_t size);
 void			free(void *ptr);
 
