c1ebb2e02ec00e9aa64896ec867efc9766350bb6
Ari <arireedy42@gmail.com>
2020-03-07 04:52:48 -0800

    adding show_alloc_mem function

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 18:47:57 by rreedy            #+#    #+#             */
-/*   Updated: 2020/03/04 20:13:30 by rreedy           ###   ########.fr       */
+/*   Updated: 2020/03/07 03:38:35 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void			*malloc(size_t size);
 void			*calloc(size_t num, size_t size);
 void			*realloc(void *ptr, size_t size);
 void			free(void *ptr);
+void			show_alloc_mem(void);
 
 #endif
---
+++ srcs/show_alloc_mem.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   show_alloc_mem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 03:03:10 by rreedy            #+#    #+#             */
+/*   Updated: 2020/03/07 04:50:59 by rreedy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+#include "malloc.h"
+#include "struct_tnysml_mmap_header.h"
+#include "struct_tnysml_alloc_header.h"
+#include "struct_lrg_alloc_header.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void	show_tny(void)
+{
+	struct s_tnysml_mmap_header		*cur;
+	unsigned int					addr;
+	unsigned int					used;
+	size_t							i;
+
+	cur = info->tny_mmaps;
+	while (cur)
+	{
+		i = 0;
+		while (i < info->n_tny_allocs_per_mmap)
+		{
+			addr = (uintptr_t)cur + info->tny_mmap_offset +
+					((info->tnysml_alheadr_siz + TNY_ALLOC_SIZE) * i);
+			used = ((struct s_tnysml_alloc_header *)
+					((uintptr_t)cur + info->tny_mmap_offset +
+					((info->tnysml_alheadr_siz + TNY_ALLOC_SIZE) * i)))->used;
+			if (!(((struct s_tnysml_alloc_header *)
+					((uintptr_t)cur + info->tny_mmap_offset +
+					((info->tnysml_alheadr_siz + TNY_ALLOC_SIZE) * i)))->free))
+				printf("%#.8x - %#.8x : %d bytes\n", addr, addr + used, used);
+			++i;
+		}
+		cur = cur->next_mmap;
+	}
+}
+
+void	show_sml(void)
+{
+	struct s_tnysml_mmap_header		*cur;
+	unsigned int					addr;
+	unsigned int					used;
+	size_t							i;
+
+	cur = info->sml_mmaps;
+	while (cur)
+	{
+		i = 0;
+		while (i < info->n_sml_allocs_per_mmap)
+		{
+			addr = (uintptr_t)cur + info->sml_mmap_offset +
+					((info->tnysml_alheadr_siz + SML_ALLOC_SIZE) * i);
+			used = ((struct s_tnysml_alloc_header *)
+					((uintptr_t)cur + info->sml_mmap_offset +
+					((info->tnysml_alheadr_siz + SML_ALLOC_SIZE) * i)))->used;
+			if (!(((struct s_tnysml_alloc_header *)
+					((uintptr_t)cur + info->sml_mmap_offset +
+					((info->tnysml_alheadr_siz + SML_ALLOC_SIZE) * i)))->free))
+				printf("%#.8x - %#.8x : %d bytes\n", addr, addr + used, used);
+			++i;
+		}
+		cur = cur->next_mmap;
+	}
+}
+
+void	show_lrg(void)
+{
+	struct s_lrg_alloc_header		*cur;
+	unsigned int					addr;
+	unsigned int					used;
+
+	cur = info->lrg_allocs;
+	while (cur)
+	{
+		addr = (uintptr_t)cur + info->lrg_alheadr_siz;
+		used = cur->used;
+		printf("%#.8x - %#.8x : %d bytes\n", addr, addr + used, used);
+		cur = cur->next_alloc;
+	}
+}
+
+void	show_alloc_mem(void)
+{
+	write(1, "TINY :\n", 7);
+	show_tny();
+	write(1, "SMALL :\n", 8);
+	show_sml();
+	write(1, "LARGE :\n", 8);
+	show_lrg();
+}
